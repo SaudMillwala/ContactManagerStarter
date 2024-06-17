f7804f295f445916e6ec253faa18c834fba469ea
SaudMillwala <saudmillwala@gmail.com>
2024-06-17 15:18:39 -0400

    added selection as string but throws error that it is null when you try to re-edit or save

--- ContactManagerStarter/Models/EditContactViewModel.cs
+++ ContactManagerStarter/Models/EditContactViewModel.cs
@@ -9,6 +9,7 @@ namespace ContactManager.Models
     public class EditContactViewModel
     {
         public Guid Id { get; set; }
+
         public string Title { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
